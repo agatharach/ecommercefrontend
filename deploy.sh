1f89b37e652d98d9000b5c5c883f996a6f76226a
agatharach <agatha@alterra.id>
2019-08-28 21:41:04 +0700

    still travis

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
-docker stop helloworld
-docker rm helloworld
-docker rmi aprynur/helloworld
-docker run -d --name helloworld -p 5000:5000 aprynur/helloworld:latest
-#aa
+docker stop react
+docker rm react
+docker rmi agatharach/react:latest
+docker run -d --name react -p 3000:80 agatharach/react:latest
