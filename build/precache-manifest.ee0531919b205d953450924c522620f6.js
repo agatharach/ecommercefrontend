0ab482720abed9de3bd16657f2c4e162fa183bdc
agatharach <agatha@alterra.id>
2019-08-28 20:04:04 +0700

    automate travis harus diluar folder

---
+++ build/precache-manifest.ee0531919b205d953450924c522620f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70e5a18cf324ec11e546c3b4bd0edcff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f891664e033aa55528f",
+    "url": "/static/css/main.e1694390.chunk.css"
+  },
+  {
+    "revision": "c754c757b249e24215f6",
+    "url": "/static/js/2.00d00cea.chunk.js"
+  },
+  {
+    "revision": "4f891664e033aa55528f",
+    "url": "/static/js/main.d8d2f490.chunk.js"
+  },
+  {
+    "revision": "4456479015a72df4dcf5",
+    "url": "/static/js/runtime~main.e264b2cb.js"
+  },
+  {
+    "revision": "77df2b6ec1ffb34f6869f6089be2d2ce",
+    "url": "/static/media/grain.77df2b6e.png"
+  }
+]);
